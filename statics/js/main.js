727499e3226e9e6b76d668d37279500830523677
soulxyz <xygzs2018@qq.com>
2019-12-18 17:55:03 +0800

    Update main.js

--- statics/js/main.js
+++ statics/js/main.js
@@ -36,7 +36,7 @@ function getAchives(){
 
 function gethitokoto(){
     $.ajax({
-        type:"POST",
+        type:"text",
         url:"https://v1.hitokoto.cn/?encode=text",
         dataType:"json",
         success:function(result){
